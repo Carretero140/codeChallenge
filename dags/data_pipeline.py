6f24a30212ba443ce04a06cc7b48f8c54b21ff99
carretero <carretero140@gmail.com>
2022-01-23 16:20:08 -0500

    added task that processes file

--- dags/data_pipeline.py
+++ dags/data_pipeline.py
@@ -1,14 +1,29 @@
 from datetime import timedelta
 from airflow import DAG
 from airflow.sensors.filesystem import FileSensor
+from airflow.operators.python import PythonOperator
 
 from datetime import datetime, timedelta
+import pandas as pd
 
 default_args = {
     "owner":"airflow",
     "retries":3,
     "retry_delay":timedelta(minutes=5)
 }
+
+def _download_trips():
+    for chunk in pd.read_csv('/opt/airflow/dags/files/trips.csv',chunksize=10):
+        chunk['origin_coord'] = chunk['origin_coord'].map(lambda x: x.strip('POINT ()')).str.split(' ')
+        chunk['origin1'] = chunk['origin_coord'].str[0]
+        chunk['origin2'] = chunk['origin_coord'].str[1]
+        chunk['destination_coord'] = chunk['destination_coord'].map(lambda x: x.strip('POINT ()')).str.split(' ')
+        chunk['destination1'] = chunk['destination_coord'].str[0]
+        chunk['destination2'] = chunk['destination_coord'].str[1]
+        columns = ['region','origin1','origin2','destination1','destination2','datetime','datasource']
+        header = False
+        chunk.to_csv('trips_processed.csv',columns=columns,header=header, mode='a',index = False)
+        
 
 with DAG("data_pipeline", start_date=datetime(2022,1,1),schedule_interval="@daily",default_args=default_args,catchup=False) as dag:
 
@@ -18,5 +33,10 @@ with DAG("data_pipeline", start_date=datetime(2022,1,1),schedule_interval="@dail
         filepath="trips.csv",
         poke_interval=5,
         timeout=20
+    )
+
+    processing_trips = PythonOperator(
+        task_id='processing_trips',
+        python_callable=_download_trips
     )
 
